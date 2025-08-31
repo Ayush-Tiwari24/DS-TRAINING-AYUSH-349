cddefb7d60426a751750a8c54d66fc6e5d009420
VaRiAbleX1 <ranjanayush2021@gmail.com>
2025-09-01 01:31:35 +0530

    Found missing number

---
+++ missingnum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, arr[100],count=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i]; 
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n;j++){
+         if(arr[j]==i){     
+              count++;
+              break;
+            }
+            
+        }
+        if(count==0){
+            cout<<i ;
+        }
+        count=0;   
+    }
+    
+    return 0;
+}
